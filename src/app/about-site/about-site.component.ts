a508dfeed01b05565f49d6b44c07dc805a0e6543
Marvin Dawson <marvintdawson@gmail.com>
2018-12-23 11:51:48 -0600

    refactor about-site.ts to call scss file

--- src/app/about-site/about-site.component.ts
+++ src/app/about-site/about-site.component.ts
@@ -4,7 +4,7 @@ import {SiteDataService} from '../../providers/site-data/site-data.service';
 @Component({
   selector: 'app-about-site',
   templateUrl: '/about-site.component.html',
-  styleUrls: ['/about-site.component.css']
+  styleUrls: ['/about-site.component.scss']
 })
 export class AboutSiteComponent {
   siteName = this.siteData.siteName;
