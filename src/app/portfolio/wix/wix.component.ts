0e80f49cccec98d5f6a259e4fac32076f1e2a40f
Marvin Dawson <marvintdawson@gmail.com>
2019-01-30 13:30:52 -0600

    rename sass folder to styles and added responsive breakpoints

--- src/app/portfolio/wix/wix.component.ts
+++ src/app/portfolio/wix/wix.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+const PROJS = [
+  {
+    '_id': 1,
+    'title': 'Reggie\'s Buckets',
+    'frameworks': 'WIX',
+    'imagePath': '../../../assets/img/clients/reggiesbucket/reggiesbucket_homescreen.jpg',
+    'summary': 'Ex MLB player, Reggie York\'s fitness improvement site.',
+    'siteUrl': 'goToReggiesBucket()'
+  }
+];
+
 @Component({
   selector: 'app-wix',
   templateUrl: './wix.component.html',
@@ -10,6 +21,10 @@ export class WixComponent implements OnInit {
 
   prePageTitle = 'Designed with';
   pageTitle = 'WIX';
+  headerImage = '../../../assets/img/site/zend_2018_php_7.jpg';
+  headerImageAlt = 'WIX Header Image';
+  projects = PROJS;
+  framework = 'WIX';
 
   constructor() { }
 
