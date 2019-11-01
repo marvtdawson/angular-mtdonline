f9818205a4f87fcacb9333c97ace97fe7184ca1f
Marvin Dawson <marvintdawson@gmail.com>
2019-11-01 06:36:16 -0500

    added firebase SDK call to the app

--- src/app/layout/bs-navbar/bs-navbar.component.ts
+++ src/app/layout/bs-navbar/bs-navbar.component.ts
@@ -4,7 +4,7 @@ import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { AdminService } from '../../admin/admin.service';
 import { environment } from '../../../environments/environment';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 import {AngularFireAuth} from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 
