3531f530db1760e5395bfe5134f0f3d4690898c6
Marvin Dawson <marvintdawson@gmail.com>
2019-11-01 06:08:16 -0500

    added firebase SDK to project index

--- config/lib/global-const.js
+++ config/lib/global-const.js
@@ -3,7 +3,7 @@ const globalConst = [
   AUTHOR_NAME = 'Marvin Dawson',
   DESIGN_COMPANY_NAME = 'Home Grown Designs LLC',
   MONGODB_PASSWORD = 'k4VvRlXICEFw8UyA',
-  MTD_ONLINE  = "http://www.mtdonline.net"
+  MTD_ONLINE  = "http://www.marvintdawson.com"
 
 ];
 
